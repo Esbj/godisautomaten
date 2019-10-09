284565c97a12879cee1c4546c7f0dddd1fb2f364
Alexander <alex.esbj@gmail.com>
2019-10-09 13:06:25 +0200

    Grund
    klasser och början på en meny

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace godisautomaten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till godisautomaten!");
+            Console.WriteLine("Japp");
+            Console.WriteLine("Daim");
+            Console.WriteLine("Marabou");
+            Console.WriteLine("Pigall");
+            
+        }
+    }
+}
---
+++ godis.cs
@@ -0,0 +1,8 @@
+namespace godisautomaten{
+    public class ammount{
+        
+    }
+    public class type {
+
+    }
+}
